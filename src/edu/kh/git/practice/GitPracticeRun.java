6da2817d326eecc8a94d1fa0cf368fefadbebe9b
jihoon <last-child@github.com>
2023-02-20 19:20:26 +0900

    문장 수정

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -3,8 +3,7 @@ package edu.kh.git.practice;
 public class GitPracticeRun {
 
 	public static void main(String[] args) {
-		System.out.println("Git 연습용 파일입니다.");
-
+		System.out.println("Git 연습용 파일입니다!!!");
 	}
 
 }
